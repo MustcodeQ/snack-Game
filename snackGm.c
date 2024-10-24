2dbc26c18199b418401b8826ad9477a62fac7e6d
MustcodeQ <167875732+MustcodeQ@users.noreply.github.com>
2024-10-24 12:51:13 +0200

    Update snackGm.c

--- snackGm.c
+++ snackGm.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>   // Include for random seed
+#include <time.h>   
 #ifdef _WIN32
     #include <conio.h>
-    #include <windows.h>  // For Sleep() on Windows
+    #include <windows.h>
 #else
     #include <unistd.h>
     #include <termios.h>
@@ -18,7 +18,6 @@ int x, y, fruitx, fruity, flag;
 int tailX[100], tailY[100];
 int nTail;
 
-// Function to set up the game
 void setup() {
     gameover = 0;
     x = HEIGHT / 2;
@@ -26,51 +25,60 @@ void setup() {
     fruitx = rand() % HEIGHT;
     fruity = rand() % WIDTH;
     score = 0;
+    nTail = 0;
 }
 
-// Function to draw the game screen
 void draw() {
     system("cls || clear");
 
-    // Draw boundaries and snake
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)
                 printf("#");
             else if (i == x && j == y)
-                printf("0");
-            else if (i == fruitx && j == fruity)
-                printf("*");
-            else
-                printf(" ");
+                printf("0"); // Head of the snake
+            else {
+                int isTail = 0;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == i && tailY[k] == j) {
+                        printf("o"); // Body of the snake
+                        isTail = 1;
+                        break;
+                    }
+                }
+                if (!isTail) {
+                    if (i == fruitx && j == fruity)
+                        printf("*"); // Fruit
+                    else
+                        printf(" ");
+                }
+            }
         }
         printf("\n");
     }
 
-    // Display score
     printf("Score: %d\n", score);
     printf("Press X to quit the game\n");
 }
 
-// Function to handle user input
 void input() {
 #ifdef _WIN32
     if (_kbhit()) {
         switch (_getch()) {
             case 'a':
-                flag = 1;
+                flag = 1; // Left
                 break;
             case 'd':
-                flag = 2;
+                flag = 2; // Right
                 break;
             case 'w':
-                flag = 3;
+                flag = 3; // Up
                 break;
             case 's':
-                flag = 4;
+                flag = 4; // Down
                 break;
             case 'x':
-                gameover = 1;
+                gameover = 1; // Quit
                 break;
         }
     }
@@ -86,19 +94,19 @@ void input() {
     
     switch (ch) {
         case 'a':
-            flag = 1;
+            flag = 1; // Left
             break;
         case 'd':
-            flag = 2;
+            flag = 2; // Right
             break;
         case 'w':
-            flag = 3;
+            flag = 3; // Up
             break;
         case 's':
-            flag = 4;
+            flag = 4; // Down
             break;
         case 'x':
-            gameover = 1;
+            gameover = 1; // Quit
             break;
     }
 #endif
@@ -106,6 +114,22 @@ void input() {
 
 // Function for game logic
 void logic() {
+    // Save current position to the tail
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
     switch (flag) {
         case 1: // Left
             y--;
@@ -122,19 +146,27 @@ void logic() {
     }
 
     // Check for collision with boundaries
-    if (x < 0 || x >= HEIGHT || y < 0 || y >= WIDTH)
+    if (x < 1 || x >= HEIGHT - 1 || y < 1 || y >= WIDTH - 1)
         gameover = 1;
 
     // Check if snake eats the fruit
     if (x == fruitx && y == fruity) {
         score += 10;
-        fruitx = rand() % HEIGHT;
-        fruity = rand() % WIDTH;
+        fruitx = rand() % (HEIGHT - 2) + 1; // Generate fruit within bounds
+        fruity = rand() % (WIDTH - 2) + 1; // Generate fruit within bounds
+        nTail++; // Increase the length of the snake
+    }
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y) {
+            gameover = 1;
+        }
     }
 }
 
 // Main function
 int main() {
+    srand(time(0)); // Seed for random number generation
     setup();
 
     // Game loop
@@ -153,4 +185,3 @@ int main() {
 
     return 0;
 }
-
